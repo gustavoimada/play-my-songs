189ad8dfb83907a1074daf19c74280d403d7ed24
gustavoimada <gu.imada@hotmail.com>
2026-03-10 10:42:41 -0300

    Initial commit

---
+++ src/main/java/unoeste/fipp/projetofciii/ProjetoFciiiApplication.java
@@ -0,0 +1,13 @@
+package unoeste.fipp.projetofciii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFciiiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoFciiiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/unoeste/fipp/projetofciii/ProjetoFciiiApplicationTests.java
@@ -0,0 +1,13 @@
+package unoeste.fipp.projetofciii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFciiiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
